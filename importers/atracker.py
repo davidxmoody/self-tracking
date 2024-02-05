1daa7de312169d8da0de51d73fee0de999080b04
David Moody <david@davidxmoody.com>
2024-02-05 14:33:02 +0000

    Use icalBuddy for importing ATracker events

--- importers/atracker.py
+++ importers/atracker.py
@@ -1,44 +1,77 @@
 # %% Imports
 
-from os import environ
+from datetime import datetime
 from os.path import expandvars
+import re
+import subprocess
 
-from dotenv import load_dotenv
-from icalendar import Calendar
 import pandas as pd
-import requests
-
-load_dotenv()
 
 
-# %% Fetch
+# %% Get events
 
-req = requests.get(environ["ATRACKER_URL"])
+event_regex = re.compile(
+    r"^• (?P<category>[A-Za-z ]+)\n"
+    r"    (?P<start_date>\d{4}-\d{2}-\d{2})"
+    r" at (?P<start_time>\d{2}:\d{2}:\d{2})"
+    r"(?P<timezone>\+\d{4})"
+    r" - (?P<end_date_optional>\d{4}-\d{2}-\d{2})?(?: at )?"
+    r"(?P<end_time>\d{2}:\d{2}:\d{2})"
+    r"\+\d{4}$",  # Second timezone is incorrect if event spans timezone boundary
+    re.MULTILINE,
+)
 
-cal = Calendar.from_ical(req.text)
 
-
-# %% Clean
-
-df = pd.DataFrame(
-    (
-        {
-            "start": pd.to_datetime(e["DTSTART"].dt),
-            "end": pd.to_datetime(e["DTEND"].dt),
-            "category": e["SUMMARY"].lower(),
-        }
-        for e in cal.walk("VEVENT")
+def get_events(year: int):
+    result = subprocess.run(
+        [
+            "icalBuddy",
+            "--dateFormat",
+            "%Y-%m-%d",
+            "--timeFormat",
+            "%H:%M:%S%z",
+            "--includeEventProps",
+            "title,datetime",
+            "-nrd",
+            "-nc",
+            "-ic",
+            "ATracker",
+            f"eventsFrom:{year}-01-01",
+            f"to:{year}-12-31",
+        ],
+        check=True,
+        capture_output=True,
+        text=True,
     )
-).sort_values("start")
 
-df["duration"] = (df.end - df.start).dt.total_seconds().astype(int)
+    df = pd.DataFrame(
+        {
+            "start": f"{m.group('start_date')} {m.group('start_time')}{m.group('timezone')}",
+            "end": f"{m.group('end_date_optional') or m.group('start_date')} {m.group('end_time')}{m.group('timezone')}",
+            "category": m.group("category").lower(),
+        }
+        for m in event_regex.finditer(result.stdout)
+    )
 
-df.loc[df.category == "side project", "category"] = "project"
+    df["start"] = df.start.astype("datetime64[s, Europe/London]")
+    df["end"] = df.end.astype("datetime64[s, Europe/London]")
+    df["duration"] = (df.end - df.start).dt.total_seconds().astype(int)
 
-included_categories = environ["ATRACKER_CATEGORIES"].split(",")
-df = df.loc[df.category.isin(included_categories)]
+    df.loc[df.category == "side project", "category"] = "project"
+    df = df.loc[df.category != "cooking"]
 
-df = df[["start", "duration", "category"]]
+    df = df.astype({"category": "category"})
+
+    df = df.sort_values("start").reset_index(drop=True)
+
+    return df[["start", "duration", "category"]]
+
+
+df = (
+    pd.concat(get_events(year) for year in range(2020, datetime.now().year + 1))
+    .drop_duplicates()
+    .reset_index(drop=True)
+)
 
 
 # %% Write
