b95c78e452932058e20a59ec26f1e5e54c3f9989
David Moody <david@davidxmoody.com>
2024-02-08 12:55:31 +0000

    Show Google location history in a heat map

--- importers/googlelocation.py
+++ importers/googlelocation.py
@@ -5,7 +5,8 @@ import json
 from os.path import expandvars
 from zipfile import ZipFile
 
-import matplotlib.pyplot as plt
+import folium
+from folium.plugins import HeatMap
 import pandas as pd
 
 
@@ -16,21 +17,25 @@ with ZipFile(filepath) as zf:
     json_data = json.load(zf.open("Takeout/Location History (Timeline)/Records.json"))
 
 df = pd.DataFrame(json_data["locations"])
-df = pd.DataFrame(
-    {
-        "datetime": pd.to_datetime(
-            df.timestamp.str.replace(r"(\.\d{3})?Z$", "", regex=True)
-        ),
-        "lat": df.latitudeE7,
-        "lon": df.longitudeE7,
-        "acc": df.accuracy,
-    }
-).drop_duplicates()
+df = (
+    pd.DataFrame(
+        {
+            "datetime": pd.to_datetime(
+                df.timestamp.str.replace(r"(\.\d{3})?Z$", "", regex=True)
+            ),
+            "lat": df.latitudeE7 / 1e7,
+            "lon": df.longitudeE7 / 1e7,
+            "accuracy": df.accuracy,
+        }
+    )
+    .drop_duplicates("datetime")
+    .set_index("datetime")
+    .query("accuracy < 50")
+)
 
 
-# %% Analyse
+# %% Heat map
 
-df.plot.scatter(x="lon", y="lat")
-plt.xlim(-6e7, 1e7)
-plt.ylim(48e7, 56e7)
-plt.show()
+map = folium.Map(zoom_start=14)
+HeatMap(df.values, blur=3, radius=10).add_to(map)
+map.show_in_browser()
