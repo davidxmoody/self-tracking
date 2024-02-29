059184b51746c4274f35f55511994b6a8ea92fee
David Moody <david@davidxmoody.com>
2024-02-29 13:07:54 +0000

    Add WIP data module

---
+++ self_tracking/data.py
@@ -0,0 +1,112 @@
+# %% Imports
+
+from datetime import timedelta
+from os.path import expandvars
+
+import pandas as pd
+
+
+# %% Helpers
+
+
+def filepath(name: str):
+    return expandvars(f"$DIARY_DIR/data/{name}.tsv")
+
+
+def read_data(name: str, parse_dates=["date"], index_col: str | None = "date"):
+    return pd.read_table(
+        filepath(name),
+        parse_dates=parse_dates,
+        index_col=index_col,
+    )
+
+
+# %% Data
+
+
+def activity():
+    return read_data("activity")
+
+
+def atracker_events():
+    df = pd.read_table(filepath("atracker"))
+    df["start"] = df.start.astype("datetime64[s, Europe/London]")
+    df["date"] = pd.to_datetime((df.start - timedelta(hours=4)).dt.date)
+    return df.loc[df.date >= "2020"]
+
+
+def climbing():
+    return read_data("climbing")
+
+
+def cycling_indoor():
+    return read_data("cycling-indoor")
+
+
+def cycling_outdoor():
+    return read_data("cycling-outdoor")
+
+
+def diet():
+    return read_data("diet")
+
+
+def eras():
+    # TODO maybe add on end and duration (in days)?
+    return read_data("eras", parse_dates=["start"], index_col=None)
+
+
+def holidays():
+    # TODO maybe add on duration (in days)?
+    return read_data("holidays", parse_dates=["start", "end"], index_col=None)
+
+
+def meditation():
+    return read_data("meditation")
+
+
+def running():
+    return read_data("running")
+
+
+def sleep():
+    # TODO maybe parse durations?
+    return read_data("sleep")
+
+
+def social():
+    # TODO this is old data and date index maybe won't work for everything
+    return read_data("social")
+
+
+def streaks():
+    # TODO maybe rename this to be streaks_events and then have a separate pivot
+    return read_data("streaks", index_col=None)
+
+
+def strength():
+    # TODO rename to strength_sets
+    return read_data("strength", index_col=None)
+
+
+def weight():
+    return read_data("weight")
+
+
+# %% Derived data
+
+
+def net_calories():
+    eaten = diet().calories
+    activity_df = activity()
+    active = activity_df.active_calories
+    basal = activity_df.basal_calories
+    return (eaten - active - basal).dropna().astype(int)
+
+
+def atracker():
+    df = atracker_events().pivot_table(
+        values="duration", index="date", columns="category", aggfunc="sum"
+    )
+    df = df.fillna(df.mask(df.ffill().notna(), 0))
+    return df.astype("Int64")  # TODO maybe use timedeltas here?
