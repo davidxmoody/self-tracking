6c61840f5f3a0ea816ac553e9b3c01646648d71d
David Moody <david@davidxmoody.com>
2024-03-13 09:14:33 +0000

    Update workout routes map

--- self_tracking/graphs/workoutroutes.py
+++ self_tracking/graphs/workoutroutes.py
@@ -1,25 +1,60 @@
 # %% Imports
 
+from dataclasses import dataclass
 from glob import glob
-from os.path import expandvars
+import gzip
+from os.path import basename, expandvars
+import xml.etree.ElementTree as ET
 
-from gpxplotter import add_segment_to_map, create_folium_map, read_gpx_file
+import folium
 
 
 # %% Load
 
-segments = []
-for f in glob(expandvars("$HOME/Downloads/apple_health_export/workout-routes/*.gpx")):
-    segments.extend(next(read_gpx_file(f))["segments"])
+
+@dataclass
+class Route:
+    date: str
+    time: str
+    activity: str
+    path: list[tuple[float, float]]
+
+    def __init__(self, filepath: str):
+        (self.date, time, self.activity) = basename(filepath)[:-7].split("_")
+        self.time = time.replace("-", ":")[:-3]
+
+        with gzip.open(filepath) as file:
+            root = ET.parse(file).getroot()
+            self.path = [
+                (float(p.attrib["lat"]), float(p.attrib["lon"]))
+                for p in root.iter()
+                if p.tag.endswith("trkpt")
+            ]
+
+
+routes = [Route(fp) for fp in glob(expandvars("$DIARY_DIR/data/routes/*.gpx.gz"))]
 
 
 # %% Graphs
 
-line_options = {"color": "red", "weight": 5, "opacity": 0.6}
+activity_colors = {
+    "running": "red",
+    "cycling": "blue",
+    "walking": "green",
+}
 
-map = create_folium_map()
+map = folium.Map(tiles="CartoDB Positron")
 
-for segment in segments:
-    add_segment_to_map(map, segment, line_options=line_options)
+for route in routes:
+    if route.activity == "running":
+        folium.PolyLine(
+            route.path,
+            weight=5,
+            opacity=0.6,
+            color=activity_colors[route.activity],
+            tooltip=f"{route.date} {route.time} {route.activity}",
+        ).add_to(map)
+
+map.fit_bounds([[51.48, -2.67], [51.42, -2.56]])
 
 map.show_in_browser()
