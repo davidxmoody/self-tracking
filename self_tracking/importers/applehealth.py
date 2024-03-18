cbb43022a96836ab2a6f195c11770de75ba3046f
David Moody <david@davidxmoody.com>
2024-03-18 11:16:10 +0000

    Refactor Apple Health importer to main form

--- self_tracking/importers/applehealth.py
+++ self_tracking/importers/applehealth.py
@@ -13,18 +13,22 @@ import pandas as pd
 
 # %% Load
 
-filepath = sorted(glob(expandvars("$HOME/Downloads/????-??-??-apple-health.zip")))[-1]
-with ZipFile(filepath) as zf:
-    root = ET.parse(zf.open("apple_health_export/export.xml")).getroot()
 
-export_date_node = root.find("./ExportDate")
-if export_date_node is None:
-    raise Exception("Could not find export date")
-export_date = date.fromisoformat(export_date_node.attrib["value"][0:10])
-last_full_day = export_date - timedelta(days=1)
+def getroot() -> ET.Element:
+    fp = sorted(glob(expandvars("$HOME/Downloads/????-??-??-apple-health.zip")))[-1]
+    with ZipFile(fp) as zf:
+        return ET.parse(zf.open("apple_health_export/export.xml")).getroot()
 
 
 # %% Helpers
+
+
+def get_last_full_day(root: ET.Element):
+    export_date_node = root.find("./ExportDate")
+    if export_date_node is None:
+        raise Exception("Could not find export date")
+    export_date = date.fromisoformat(export_date_node.attrib["value"][0:10])
+    return export_date - timedelta(days=1)
 
 
 def parse_date(node):
@@ -62,7 +66,9 @@ def sum_by_date(df):
     return cast(pd.DataFrame, df.groupby("date").agg("sum"))
 
 
-def gather_records(rtype: str, unit: str, sources: list[str], agg="sum"):
+def gather_records(
+    root: ET.Element, rtype: str, unit: str, sources: list[str], agg="sum"
+):
     df = pd.DataFrame(
         {
             "date": node.attrib["endDate"][0:10],
@@ -76,129 +82,166 @@ def gather_records(rtype: str, unit: str, sources: list[str], agg="sum"):
     return series
 
 
-def read_table_with_date_index(filename: str):
-    return pd.read_table(filename, parse_dates=["date"], index_col="date")
+def write_tsv(df: pd.DataFrame, name: str):
+    output_filename = expandvars(f"$DIARY_DIR/data/{name}.tsv")
+    df.to_csv(output_filename, sep="\t", float_format="%.2f")
 
 
 # %% Running
 
-running_manual = read_table_with_date_index(
-    expandvars("$DIARY_DIR/misc/2024-02-14-old-running.tsv")
-)
 
-running_garmin = pd.read_csv(
-    expandvars("$DIARY_DIR/misc/2024-02-14-garmin-export.csv")
-).query("`Activity Type` == 'Running'")
+def extract_running(root: ET.Element) -> None:
+    running_manual = pd.read_table(
+        expandvars("$DIARY_DIR/misc/2024-02-14-old-running.tsv"),
+        parse_dates=["date"],
+        index_col="date",
+    )
 
-running_garmin = pd.DataFrame(
-    {
-        "date": pd.to_datetime(running_garmin.Date.str[0:10]),
-        "distance": running_garmin.Distance,
-        "calories": running_garmin.Calories.str.replace(",", "").astype(int),
-        "duration": running_garmin.Time.replace(r"\.\d*", "", regex=True).apply(
-            lambda v: sum(int(x) * 60 ** (1 - i) for i, x in enumerate(v.split(":")))
-        ),
-    }
-).set_index("date")
+    running_garmin = pd.read_csv(
+        expandvars("$DIARY_DIR/misc/2024-02-14-garmin-export.csv")
+    ).query("`Activity Type` == 'Running'")
 
-running_apple = sum_by_date(
-    pd.DataFrame(
+    running_garmin = pd.DataFrame(
         {
-            "date": parse_date(node),
-            "distance": parse_distance(node, "WalkingRunning"),
-            "calories": parse_calories(node),
-            "duration": parse_duration(node),
+            "date": pd.to_datetime(running_garmin.Date.str[0:10]),
+            "distance": running_garmin.Distance,
+            "calories": running_garmin.Calories.str.replace(",", "").astype(int),
+            "duration": running_garmin.Time.replace(r"\.\d*", "", regex=True).apply(
+                lambda v: sum(
+                    int(x) * 60 ** (1 - i) for i, x in enumerate(v.split(":"))
+                )
+            ),
         }
-        for node in root.iterfind(
-            "./Workout[@workoutActivityType='HKWorkoutActivityTypeRunning']"
+    ).set_index("date")
+
+    running_apple = sum_by_date(
+        pd.DataFrame(
+            {
+                "date": parse_date(node),
+                "distance": parse_distance(node, "WalkingRunning"),
+                "calories": parse_calories(node),
+                "duration": parse_duration(node),
+            }
+            for node in root.iterfind(
+                "./Workout[@workoutActivityType='HKWorkoutActivityTypeRunning']"
+            )
         )
     )
-)
 
-running = (
-    pd.concat([running_manual, running_garmin, running_apple])
-    .astype({"calories": "Int64"})
-    .sort_index()
-)
+    running = (
+        pd.concat([running_manual, running_garmin, running_apple])
+        .astype({"calories": "Int64"})
+        .sort_index()
+    )
+
+    write_tsv(running, "running")
 
 
 # %% Cycling
 
-cycling_mixed = pd.DataFrame(
-    {
-        "date": parse_date(node),
-        "calories": parse_calories(node),
-        "duration": parse_duration(node),
-        "distance": parse_distance(node, "Cycling"),
-        "indoor": parse_indoor(node),
-    }
-    for node in root.iterfind(
-        "./Workout[@workoutActivityType='HKWorkoutActivityTypeCycling']"
+
+def extract_cycling(root: ET.Element) -> None:
+    cycling_mixed = pd.DataFrame(
+        {
+            "date": parse_date(node),
+            "calories": parse_calories(node),
+            "duration": parse_duration(node),
+            "distance": parse_distance(node, "Cycling"),
+            "indoor": parse_indoor(node),
+        }
+        for node in root.iterfind(
+            "./Workout[@workoutActivityType='HKWorkoutActivityTypeCycling']"
+        )
     )
-)
 
-cycling_indoor = sum_by_date(
-    cycling_mixed[cycling_mixed["indoor"] == True].drop(["distance", "indoor"], axis=1)
-)
+    cycling_indoor = sum_by_date(
+        cycling_mixed[cycling_mixed["indoor"] == True].drop(
+            ["distance", "indoor"], axis=1
+        )
+    )
 
-cycling_outdoor = sum_by_date(
-    cycling_mixed[cycling_mixed["indoor"] == False].drop(["indoor"], axis=1)
-)
+    cycling_outdoor = sum_by_date(
+        cycling_mixed[cycling_mixed["indoor"] == False].drop(["indoor"], axis=1)
+    )
+
+    write_tsv(cycling_indoor, "cycling-indoor")
+    write_tsv(cycling_outdoor, "cycling-outdoor")
 
 
 # %% Activity
 
-activity_sources = ["David’s Apple\xa0Watch"]
 
-activity = pd.concat(
-    {
-        "active_calories": gather_records(
-            "ActiveEnergyBurned", "Cal", activity_sources
-        ),
-        "basal_calories": gather_records("BasalEnergyBurned", "kcal", activity_sources),
-    },
-    axis=1,
-)
+def extract_activity(root: ET.Element) -> None:
+    activity_sources = ["David’s Apple\xa0Watch"]
 
-activity = activity.loc["2017-12-16":last_full_day].round(0).astype(int)
+    activity = pd.concat(
+        {
+            "active_calories": gather_records(
+                root, "ActiveEnergyBurned", "Cal", activity_sources
+            ),
+            "basal_calories": gather_records(
+                root, "BasalEnergyBurned", "kcal", activity_sources
+            ),
+        },
+        axis=1,
+    )
+
+    activity = activity.loc["2017-12-16" : get_last_full_day(root)].round(0).astype(int)
+
+    write_tsv(activity, "activity")
 
 
 # %% Diet
 
-diet_sources = ["Calorie Counter", "YAZIO", "MyNetDiary"]
 
-diet = pd.concat(
-    {
-        "calories": gather_records("DietaryEnergyConsumed", "Cal", diet_sources),
-        "protein": gather_records("DietaryProtein", "g", diet_sources),
-        "fat": gather_records("DietaryFatTotal", "g", diet_sources),
-        "carbs": gather_records("DietaryCarbohydrates", "g", diet_sources),
-        "sugar": gather_records("DietarySugar", "g", diet_sources),
-        "fiber": gather_records("DietaryFiber", "g", diet_sources),
-    },
-    axis=1,
-).astype(int)
+def extract_diet(root: ET.Element) -> None:
+    diet_sources = ["Calorie Counter", "YAZIO", "MyNetDiary"]
 
-diet = diet.loc[:last_full_day]
+    diet = pd.concat(
+        {
+            "calories": gather_records(
+                root, "DietaryEnergyConsumed", "Cal", diet_sources
+            ),
+            "protein": gather_records(root, "DietaryProtein", "g", diet_sources),
+            "fat": gather_records(root, "DietaryFatTotal", "g", diet_sources),
+            "carbs": gather_records(root, "DietaryCarbohydrates", "g", diet_sources),
+            "sugar": gather_records(root, "DietarySugar", "g", diet_sources),
+            "fiber": gather_records(root, "DietaryFiber", "g", diet_sources),
+        },
+        axis=1,
+    ).astype(int)
+
+    diet = diet.loc[: get_last_full_day(root)]
+
+    write_tsv(diet, "diet")
 
 
 # %% Weight
 
-weight_sources = ["Withings"]
 
-weight_new = pd.concat(
-    {
-        "weight": gather_records("BodyMass", "lb", weight_sources, agg="min"),
-        "fat": gather_records("BodyFatPercentage", "%", weight_sources, agg="min"),
-    },
-    axis=1,
-)
+def extract_weight(root: ET.Element) -> None:
+    weight_sources = ["Withings"]
 
-weight_old = read_table_with_date_index(
-    expandvars("$DIARY_DIR/misc/2024-01-22-old-weights.tsv")
-)
+    weight_new = pd.concat(
+        {
+            "weight": gather_records(root, "BodyMass", "lb", weight_sources, agg="min"),
+            "fat": gather_records(
+                root, "BodyFatPercentage", "%", weight_sources, agg="min"
+            ),
+        },
+        axis=1,
+    )
 
-weight = pd.concat([weight_old, weight_new])
+    weight_old = pd.read_table(
+        expandvars("$DIARY_DIR/misc/2024-01-22-old-weights.tsv"),
+        parse_dates=["date"],
+        index_col="date",
+    )
+
+    weight = pd.concat([weight_old, weight_new])
+    weight["fat"] = weight.fat.map(lambda v: "" if isnan(v) else format(v, ".3f"))
+
+    write_tsv(weight, "weight")
 
 
 # %% Meditation
@@ -210,69 +253,72 @@ def parse_mindful_minutes(node):
     return round((end - start).total_seconds() / 60)
 
 
-meditation = sum_by_date(
-    pd.DataFrame(
-        {
-            "date": parse_date(node),
-            "mindful_minutes": parse_mindful_minutes(node),
-        }
-        for node in root.iterfind(
-            "./Record[@type='HKCategoryTypeIdentifierMindfulSession']"
+def extract_meditation(root: ET.Element) -> None:
+    meditation = sum_by_date(
+        pd.DataFrame(
+            {
+                "date": parse_date(node),
+                "mindful_minutes": parse_mindful_minutes(node),
+            }
+            for node in root.iterfind(
+                "./Record[@type='HKCategoryTypeIdentifierMindfulSession']"
+            )
         )
     )
-)
+
+    write_tsv(meditation, "meditation")
 
 
 # %% Sleep
 
-sleep_sources = ["David’s Apple\xa0Watch"]
 
-sleep = pd.DataFrame(
-    {
-        "source": x.attrib["sourceName"],
-        "start": pd.to_datetime(x.attrib["startDate"]),
-        "end": pd.to_datetime(x.attrib["endDate"]),
-        "subtype": x.attrib["value"][28:],
-    }
-    for x in root.iterfind("./Record[@type='HKCategoryTypeIdentifierSleepAnalysis']")
-)
+def extract_sleep(root: ET.Element) -> None:
+    sleep_sources = ["David’s Apple\xa0Watch"]
 
-# Note: A lot of potential data in an older format is being discarded here.
-sleep = sleep.loc[
-    sleep.source.isin(sleep_sources)
-    & ~sleep.subtype.isin(["AsleepUnspecified", "InBed"])
-]
-
-sleep["subtype"] = sleep.subtype.str.replace("Asleep", "")
-
-sleep["duration"] = (sleep.end - sleep.start).dt.total_seconds()
-
-sleep["date"] = pd.to_datetime((sleep.end + timedelta(hours=8)).dt.date)
-
-sleep_pivot = sleep.pivot_table(
-    index="date", columns="subtype", values="duration", aggfunc="sum"
-).astype(int)[["Deep", "Core", "REM", "Awake"]]
-
-
-# %% Write TSV files
-
-
-def write_tsv(df: pd.DataFrame, name: str, precisions: dict[str, int] = {}):
-    df = df.copy()
-    for col, p in precisions.items():
-        df[col] = df[col].apply(
-            lambda v: "" if isnan(float(v)) else format(float(v), f".{p}f")
+    sleep = pd.DataFrame(
+        {
+            "source": x.attrib["sourceName"],
+            "start": pd.to_datetime(x.attrib["startDate"]),
+            "end": pd.to_datetime(x.attrib["endDate"]),
+            "subtype": x.attrib["value"][28:],
+        }
+        for x in root.iterfind(
+            "./Record[@type='HKCategoryTypeIdentifierSleepAnalysis']"
         )
+    )
 
-    output_filename = expandvars(f"$DIARY_DIR/data/{name}.tsv")
-    df.to_csv(output_filename, sep="\t", float_format="%.2f")
+    # Note: A lot of potential data in an older format is being discarded here.
+    sleep = sleep.loc[
+        sleep.source.isin(sleep_sources)
+        & ~sleep.subtype.isin(["AsleepUnspecified", "InBed"])
+    ]
+
+    sleep["subtype"] = sleep.subtype.str.replace("Asleep", "")
+
+    sleep["duration"] = (sleep.end - sleep.start).dt.total_seconds()
+
+    sleep["date"] = pd.to_datetime((sleep.end + timedelta(hours=8)).dt.date)
+
+    sleep_pivot = sleep.pivot_table(
+        index="date", columns="subtype", values="duration", aggfunc="sum"
+    ).astype(int)[["Deep", "Core", "REM", "Awake"]]
+
+    write_tsv(sleep_pivot, "sleep")
 
 
-write_tsv(running, "running")
-write_tsv(cycling_indoor, "cycling-indoor")
-write_tsv(cycling_outdoor, "cycling-outdoor")
-write_tsv(weight, "weight", {"fat": 3})
-write_tsv(activity, "activity")
-write_tsv(diet, "diet")
-write_tsv(meditation, "meditation")
-write_tsv(sleep_pivot, "sleep")
+# %% Main
+
+
+def main():
+    root = getroot()
+    extract_running(root)
+    extract_cycling(root)
+    extract_activity(root)
+    extract_diet(root)
+    extract_weight(root)
+    extract_meditation(root)
+    extract_sleep(root)
+
+
+if __name__ == "__main__":
+    main()
