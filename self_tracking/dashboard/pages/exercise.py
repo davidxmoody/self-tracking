b16f8e76539fe5573fe758d684531f6ee4da1391
David Moody <david@davidxmoody.com>
2024-11-15 13:38:52 +0000

    Add period selector to exercise chart

--- self_tracking/dashboard/pages/exercise.py
+++ self_tracking/dashboard/pages/exercise.py
@@ -1,49 +1,80 @@
 import dash
-from dash import dcc
+from dash import Input, Output, dcc, html
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import self_tracking.data as d
+import dash_mantine_components as dmc
 
 dash.register_page(__name__)
 
-rule = "MS"
 
-r = d.running().distance.resample(rule).sum()
-co = d.cycling_outdoor().calories.resample(rule).sum()
-ci = d.cycling_indoor().calories.resample(rule).sum()
-s = d.strength().drop_duplicates("date").set_index("date").title.resample(rule).size()
-c = d.climbing().place.resample(rule).size()
+aggregation_periods = {
+    "Daily": "D",
+    "Weekly": "W-MON",
+    "Monthly": "MS",
+    "Quarterly": "QS",
+    "Yearly": "YS",
+}
 
-fig = make_subplots(
-    rows=4,
-    cols=1,
-    shared_xaxes=True,
-    subplot_titles=["Running", "Cycling", "Strength training", "Climbing"],
+layout = html.Div(
+    [
+        dmc.RadioGroup(
+            id="exercise-aggregation-period",
+            children=[dmc.Radio(k, value=v) for k, v in aggregation_periods.items()],
+            value=aggregation_periods["Monthly"],
+            label="Aggregation period:",
+            size="sm",
+        ),
+        dcc.Graph(id="exercise-bar-chart", figure={}),
+    ]
 )
 
-fig.add_trace(go.Bar(x=r.index, y=r.values, name="Running"), row=1, col=1)
-fig.update_yaxes(title_text="Miles", range=[0, 80], row=1, col=1)
 
-fig.add_trace(
-    go.Bar(x=co.index, y=co.values, name="Outdoor"),
-    row=2,
-    col=1,
+@dash.callback(
+    Output("exercise-bar-chart", "figure"),
+    [Input("exercise-aggregation-period", "value")],
 )
-fig.add_trace(
-    go.Bar(x=ci.index, y=ci.values, name="Indoor"),
-    row=2,
-    col=1,
-)
-fig.update_yaxes(
-    title_text="Calories", range=[0, 6000], row=2, col=1, tickformat=",.0s"
-)
+def update_graph(rule: str):
+    r = d.running().distance.resample(rule).sum()
+    co = d.cycling_outdoor().calories.resample(rule).sum()
+    ci = d.cycling_indoor().calories.resample(rule).sum()
+    s = (
+        d.strength()
+        .drop_duplicates("date")
+        .set_index("date")
+        .title.resample(rule)
+        .size()
+    )
+    c = d.climbing().place.resample(rule).size()
 
-fig.add_trace(go.Bar(x=s.index, y=s.values, name="Strength"), row=3, col=1)
-fig.update_yaxes(title_text="Sessions", range=[0, 20], row=3, col=1)
+    fig = make_subplots(
+        rows=4,
+        cols=1,
+        shared_xaxes=True,
+        subplot_titles=["Running", "Cycling", "Strength training", "Climbing"],
+    )
 
-fig.add_trace(go.Bar(x=c.index, y=c.values, name="Climbing"), row=4, col=1)
-fig.update_yaxes(title_text="Sessions", range=[0, 10], row=4, col=1)
+    fig.add_trace(go.Bar(x=r.index, y=r.values, name="Running"), row=1, col=1)
+    fig.update_yaxes(title_text="Miles", row=1, col=1)
 
-fig.update_layout(barmode="stack", showlegend=False)
+    fig.add_trace(
+        go.Bar(x=co.index, y=co.values, name="Outdoor"),
+        row=2,
+        col=1,
+    )
+    fig.add_trace(
+        go.Bar(x=ci.index, y=ci.values, name="Indoor"),
+        row=2,
+        col=1,
+    )
+    fig.update_yaxes(title_text="Calories", row=2, col=1, tickformat=",.0s")
 
-layout = dcc.Graph(figure=fig, style={"height": "600px"})
+    fig.add_trace(go.Bar(x=s.index, y=s.values, name="Strength"), row=3, col=1)
+    fig.update_yaxes(title_text="Sessions", row=3, col=1)
+
+    fig.add_trace(go.Bar(x=c.index, y=c.values, name="Climbing"), row=4, col=1)
+    fig.update_yaxes(title_text="Sessions", row=4, col=1)
+
+    fig.update_layout(barmode="stack", showlegend=False)
+
+    return fig
