561370183e9b4a8208c74895765b815a138cb0e6
David Moody <david@davidxmoody.com>
2025-03-22 12:26:52 +0000

    Import GPX routes from HealthExport Shortcut

--- self_tracking/importers/gpxroutes.py
+++ self_tracking/importers/gpxroutes.py
@@ -1,15 +1,29 @@
 import gzip
-from os.path import basename, expandvars
-from zipfile import ZipFile
+from os.path import expandvars
+from pathlib import Path
+
+import_dir = Path(
+    expandvars("$HOME/Library/Mobile Documents/com~apple~CloudDocs/Health/Routes")
+)
+
+export_dir = Path(expandvars("$DIARY_DIR/data/routes"))
 
 
 def main():
-    with ZipFile(expandvars("$HOME/Downloads/Export.zip")) as zipfile:
-        for route in zipfile.namelist():
-            filename = basename(route).replace(" ", "_").lower() + ".gz"
-            filepath = expandvars(f"$DIARY_DIR/data/routes/{filename}")
-            with gzip.open(filepath, "wb") as file:
-                file.write(zipfile.read(route))
+    skip_count = 0
+
+    for import_file in import_dir.glob("*.gpx"):
+        export_file = export_dir / (import_file.name.replace(" ", "_").lower() + ".gz")
+
+        if export_file.exists():
+            skip_count += 1
+            continue
+
+        print(f"Importing: {export_file.name}")
+        with import_file.open("rb") as f_in, gzip.open(export_file, "wb") as f_out:
+            f_out.writelines(f_in)
+
+    print(f"Skipped {skip_count} existing routes")
 
 
 if __name__ == "__main__":
