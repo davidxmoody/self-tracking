019373dfcbf76e98f95239d49db92fc85276c5d6
David Moody <david@davidxmoody.com>
2025-04-14 16:35:25 +0100

    Rename gpxroutes to routes

--- self_tracking/importers/all.py
+++ self_tracking/importers/all.py
@@ -1,12 +1,12 @@
 from .atracker import main as atracker_main
-from .gpxroutes import main as gpxroutes_main
+from .routes import main as routes_main
 from .strong import main as strong_main
 from .layers import main as layers_main
 
 
 def main():
     atracker_main()
-    gpxroutes_main()
+    routes_main()
     strong_main()
     layers_main()
 
