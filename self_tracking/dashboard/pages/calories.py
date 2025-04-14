614d564c11c9112dde24301e63dafa05e2e26302
David Moody <david@davidxmoody.com>
2025-04-14 10:41:18 +0100

    Fix calories chart

--- self_tracking/dashboard/pages/calories.py
+++ self_tracking/dashboard/pages/calories.py
@@ -1,24 +1,16 @@
 import dash
 from dash import dcc
 import self_tracking.data as d
-# from plotly_calplot import calplot
+import plotly.express as px
 
 
 dash.register_page(__name__)
 
 
-# def calories_figure():
-#     net_calories = d.net_calories().rename("net_calories").reset_index()
+def calories_figure():
+    net_calories = d.net_calories().rename("net_calories")
 
-#     return calplot(
-#         net_calories,
-#         x="date",
-#         y="net_calories",
-#         colorscale="RdBu",
-#         cmap_min=-1200,
-#         cmap_max=1200,
-#     )
+    return px.bar(net_calories.resample("MS").sum())
 
 
-# layout = dcc.Graph(figure=calories_figure())
-layout = dcc.Graph(figure={})
+layout = dcc.Graph(figure=calories_figure())
