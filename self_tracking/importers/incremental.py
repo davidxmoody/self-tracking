2f9fab9b83ed52b54bf691e8577aafe2d17469fd
David Moody <david@davidxmoody.com>
2025-05-17 08:49:57 +0100

    Add incremental eaten calories

--- self_tracking/importers/incremental.py
+++ self_tracking/importers/incremental.py
@@ -17,22 +17,43 @@ def get_latest_and_cleanup(dir: Path, pattern: str = "*.txt"):
 
 
 def activity():
-    file = get_latest_and_cleanup(health_dir / "ActiveEnergy")
-    if not file:
+    import_file = get_latest_and_cleanup(health_dir / "ActiveEnergy")
+    if not import_file:
         return 0
 
-    df = pd.read_csv(file)
+    df = pd.read_csv(import_file)
     df = df.rename(
         columns={"Date": "date", "Active energy burned(Cal)": "active_calories"}
     )
     df = df.iloc[:-1]
     df["active_calories"] = df.active_calories.round(0).astype(int)
 
-    existing_df = pd.read_table(data_dir / "activity.tsv")
+    data_file = data_dir / "activity.tsv"
+    existing_df = pd.read_table(data_file)
     old_size = existing_df.shape[0]
 
     new_df = pd.concat([existing_df, df.loc[~df.date.isin(existing_df.date)]])
-    new_df.to_csv(data_dir / "activity.tsv", index=False, sep="\t")
+    new_df.to_csv(data_file, index=False, sep="\t")
+
+    return new_df.shape[0] - old_size
+
+
+def eaten():
+    import_file = get_latest_and_cleanup(health_dir / "EatenEnergy")
+    if not import_file:
+        return 0
+
+    df = pd.read_csv(import_file)
+    df = df.rename(columns={"Date": "date", "Energy consumed(Cal)": "calories"})
+    df = df.iloc[:-1]
+    df["calories"] = df.calories.round(0).astype(int)
+
+    data_file = data_dir / "diet.tsv"
+    existing_df = pd.read_table(data_file, dtype=str)
+    old_size = existing_df.shape[0]
+
+    new_df = pd.concat([existing_df, df.loc[~df.date.isin(existing_df.date)]])
+    new_df.to_csv(data_file, index=False, sep="\t")
 
     return new_df.shape[0] - old_size
 
@@ -41,6 +62,7 @@ def main():
     with yaspin(text="Apple Health incremental") as spinner:
         count = 0
         count += activity()
+        count += eaten()
 
         spinner.text += f" ({count} records)"
         spinner.ok("✔")
